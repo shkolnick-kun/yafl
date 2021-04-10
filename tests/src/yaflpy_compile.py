71e7a7afab5552ddcabc29509c8f857219c037ef
shkolnick-kun <shkolnick.kun@gmail.com>
2021-04-10 22:23:37 +0500

    Updated (c) notices.

--- tests/src/yaflpy_compile.py
+++ tests/src/yaflpy_compile.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-    Copyright 2020 anonimous <shkolnick-kun@gmail.com> and contributors.
+    Copyright 2021 anonimous <shkolnick-kun@gmail.com> and contributors.
 
     Licensed under the Apache License, Version 2.0 (the "License");
     you may not use this file except in compliance with the License.
